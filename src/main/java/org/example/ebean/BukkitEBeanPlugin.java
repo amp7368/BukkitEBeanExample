81fd1f296107d1f291b5cc431bc28cf66272a602
amp7368 <47309189+amp7368@users.noreply.github.com>
2023-01-02 20:09:14 -0500

    make the project a minecraft plugin

---
+++ src/main/java/org/example/ebean/BukkitEBeanPlugin.java
@@ -0,0 +1,11 @@
+package org.example.ebean;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitEBeanPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello World!");
+    }
+}
