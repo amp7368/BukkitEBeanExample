3505736c82d53e626c27c43749960dc1dbc10495
amp7368 <47309189+amp7368@users.noreply.github.com>
2023-01-02 20:24:52 -0500

    set up a sqlite database, and connect with ebean

--- src/main/java/org/example/ebean/BukkitEBeanPlugin.java
+++ src/main/java/org/example/ebean/BukkitEBeanPlugin.java
@@ -1,11 +1,29 @@
 package org.example.ebean;
 
+import java.io.File;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.example.ebean.database.DatabaseSetup;
 
 public class BukkitEBeanPlugin extends JavaPlugin {
 
+    private static BukkitEBeanPlugin instance;
+
+    public BukkitEBeanPlugin() {
+        instance = this;
+    }
+
+    public static BukkitEBeanPlugin get() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
-        getLogger().info("Hello World!");
+        DatabaseSetup.load();
+    }
+
+    public File getFile(String childFile) {
+        File dataFolder = getDataFolder();
+        if (!dataFolder.exists()) dataFolder.mkdir();
+        return new File(dataFolder, childFile);
     }
 }
---
+++ src/main/java/org/example/ebean/database/DatabaseSetup.java
@@ -0,0 +1,49 @@
+package org.example.ebean.database;
+
+import io.ebean.DatabaseFactory;
+import io.ebean.config.DatabaseConfig;
+import io.ebean.datasource.DataSourceConfig;
+import java.io.File;
+import org.example.ebean.BukkitEBeanPlugin;
+
+public class DatabaseSetup {
+
+    public static void load() {
+        DataSourceConfig dataSourceConfig = configureDataSource();
+        DatabaseConfig dbConfig = configureDatabase(dataSourceConfig);
+
+        // We should use the classloader that loaded this plugin
+        // because this plugin has our ebean dependencies
+        ClassLoader cl = BukkitEBeanPlugin.class.getClassLoader();
+
+        DatabaseFactory.createWithContextClassLoader(dbConfig, cl);
+        BukkitEBeanPlugin.get().getLogger().info("Successfully created database");
+    }
+
+    private static DatabaseConfig configureDatabase(DataSourceConfig dataSourceConfig) {
+        DatabaseConfig dbConfig = new DatabaseConfig();
+        dbConfig.setDataSourceConfig(dataSourceConfig);
+        dbConfig.setDdlGenerate(true);
+        dbConfig.setDdlRun(true);
+        return dbConfig;
+    }
+
+    private static DataSourceConfig configureDataSource() {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUrl(getUrl());
+
+        // There is an error about no username/password
+        // Note that sqlite does not use a username/password
+        dataSourceConfig.setUsername("whyisthisneeeded");
+        dataSourceConfig.setPassword("whyisthisneeeded");
+
+        dataSourceConfig.setPlatform("sqlite");
+        return dataSourceConfig;
+    }
+
+    private static String getUrl() {
+        File databaseFile = BukkitEBeanPlugin.get().getFile("ExampleDb.sqlite");
+        return "jdbc:sqlite:" + databaseFile.getAbsolutePath();
+    }
+
+}
