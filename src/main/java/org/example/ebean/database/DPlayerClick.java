d1db162cabda1fdcf255bc5dc3a817dd06875500
amp7368 <47309189+amp7368@users.noreply.github.com>
2023-01-02 21:47:16 -0500

    isolate problem to a querybean issue

--- src/main/java/org/example/ebean/database/DPlayerClick.java
+++ src/main/java/org/example/ebean/database/DPlayerClick.java
@@ -1,16 +1,24 @@
 package org.example.ebean.database;
 
+import io.ebean.annotation.Identity;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.Id;
 import org.bukkit.event.block.Action;
 
 @Entity
 public class DPlayerClick {
 
+    @Id
+    @Identity
+    private long id;
     @Column
     private final Timestamp timestamp;
     @Column
@@ -19,9 +27,21 @@ public class DPlayerClick {
     @Enumerated(EnumType.STRING)
     private final Action action;
 
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+
     public DPlayerClick(String playerName, Action action) {
         this.timestamp = Timestamp.from(Instant.now());
         this.playerName = playerName;
         this.action = action;
+    }
+
+    @Override
+    public String toString() {
+        String prettyDate = dateFormatter.format(timestamp.toInstant().atZone(ZoneId.of("GMT")));
+        return "%s performed %s at %s".formatted(playerName, action, prettyDate);
+    }
+
+    public long getId() {
+        return id;
     }
 }
--- src/main/java/org/example/ebean/listener/PlayerClickListener.java
+++ src/main/java/org/example/ebean/listener/PlayerClickListener.java
@@ -1,6 +1,7 @@
 package org.example.ebean.listener;
 
 import io.ebean.DB;
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +9,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.example.ebean.BukkitEBeanPlugin;
 import org.example.ebean.database.DPlayerClick;
+import org.example.ebean.database.FindByQueryBean;
 
 public class PlayerClickListener implements Listener {
 
@@ -19,8 +21,28 @@ public class PlayerClickListener implements Listener {
     public void onPlayerClick(PlayerInteractEvent event) {
         String playerName = event.getPlayer().getName();
         Action action = event.getAction();
-        BukkitEBeanPlugin.get().getLogger().info(playerName + " clicked: " + action);
-        DPlayerClick dbEntity = new DPlayerClick(playerName, action);
-        DB.save(dbEntity);
+
+        saveClick(playerName, action);
+
+        DPlayerClick lastClick = FindByQueryBean.lastClick(playerName);
+        event.getPlayer().sendMessage(lastClick.toString());
     }
+
+    private static void saveClick(String playerName, Action action) {
+        Logger logger = BukkitEBeanPlugin.get().getLogger();
+
+        // save the click to the database
+        DPlayerClick click = new DPlayerClick(playerName, action);
+        logger.info(String.format("Saving now! - %s", click));
+        DB.save(click);
+
+        // find the click that was just saved
+        click = DB.find(DPlayerClick.class, click.getId());
+        if (click == null) {
+            logger.severe("Could not find the just saved click!");
+        } else {
+            logger.info(String.format("Successfully saved click! - %s", click));
+        }
+    }
+
 }
